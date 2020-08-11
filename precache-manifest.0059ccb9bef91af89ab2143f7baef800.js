61419d752d77436cd3d1adbfe098ce7575182a6a
unknown <lenovo@DESKTOP-DKDSO3N.utc.com>
2020-08-11 21:25:43 +0530

    Updates

---
+++ precache-manifest.0059ccb9bef91af89ab2143f7baef800.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b188c3f65a7258140852b87955295149",
+    "url": "/website1/index.html"
+  },
+  {
+    "revision": "83318b175daed541cc8e",
+    "url": "/website1/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "bfe862ce053d5ae4e67e",
+    "url": "/website1/static/css/main.d85aed81.chunk.css"
+  },
+  {
+    "revision": "83318b175daed541cc8e",
+    "url": "/website1/static/js/2.567d8b20.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/website1/static/js/2.567d8b20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfe862ce053d5ae4e67e",
+    "url": "/website1/static/js/main.c2eaa3a7.chunk.js"
+  },
+  {
+    "revision": "9aba5923b3ca923a592e",
+    "url": "/website1/static/js/runtime-main.c1a17e83.js"
+  },
+  {
+    "revision": "f6138f0db1d04f8536cfe42544915eda",
+    "url": "/website1/static/media/image2.f6138f0d.png"
+  },
+  {
+    "revision": "ab3560451b919f4ac0c0c00a3b53a3e7",
+    "url": "/website1/static/media/image3.ab356045.png"
+  },
+  {
+    "revision": "57e269d772efb557362226367e9e2a63",
+    "url": "/website1/static/media/image4.57e269d7.png"
+  },
+  {
+    "revision": "14b230eb489bb2eba2685fec486ff8be",
+    "url": "/website1/static/media/image5.14b230eb.png"
+  },
+  {
+    "revision": "577e7a4c5441b014a39a9414f61ca5b4",
+    "url": "/website1/static/media/image6.577e7a4c.png"
+  },
+  {
+    "revision": "de43ec2e31ea2c05fa99e8d648d31038",
+    "url": "/website1/static/media/soft1.de43ec2e.png"
+  }
+]);
